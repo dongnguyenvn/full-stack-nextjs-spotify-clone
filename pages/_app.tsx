02977eb38e7aa6931deca930f0bbf1f4c913cd19
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2022-01-09 07:09:04 +0700

    fix structure folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import { themes } from './theme'
-import PlayerLayout from './components/PlayerLayout'
+import { themes } from '../theme'
+import PlayerLayout from '../components/PlayerLayout'
 
 const theme = extendTheme(themes)
 
